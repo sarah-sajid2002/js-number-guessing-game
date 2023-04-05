413e53eb5a3d710f894c64de00c40cbf7352aa9f
Sarah Sajid <sarahsajid19@gmail.com>
2023-04-05 09:05:41 +0500

    js number guessing game

---
+++ guessing number in js/app.js
@@ -0,0 +1,20 @@
+let number = 30;
+
+let a = prompt("enter a numer! lets see if you guess on first try");
+
+a = Number.parseInt(a);
+let s = 1;
+while (a != number){
+    if (a > number){
+        alert("your number is greater try guessing a smaller one!")
+    }
+    if (a < number){
+        alert("your number is smaller try guessing a bigger one!")
+    }
+    a = prompt("enter no once again , hope you'll grt it right this time ")
+    s = s+1;
+}
+
+alert("wow , you guessed it right on " + s +" try")
+
+
